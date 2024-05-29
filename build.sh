df96d7e56e7b2edae7f7e9a46a8bee1d481224a4
Ravi Bagdi <ravibagdi799@outlook.com>
2024-05-29 09:41:24 +0530

    update build.sh add DEBUG

--- build.sh
+++ build.sh
@@ -8,5 +8,6 @@ poetry install
 # Set environment variable to disable collectstatic
 # python manage.py collectstatic --no-input
 export DISABLE_COLLECTSTATIC=1
+export DEBUG="False"
 
 python manage.py migrate
